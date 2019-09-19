bbd3b98cfb1a13229a436deb90af39854a6f59f3
Tashunya <tashunya_17@mail.ru>
2019-09-19 17:23:03 +0300

    Add 404 page template, add pagination test

--- core/views.py
+++ core/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Movie
 
 
 class MovieList(ListView):
     """
-    creates Movie List view
+    creates view for all the movies
     """
     model = Movie
+    paginate_by = 10
 
+
+class MovieDetail(DetailView):
+    """
+    creates view for one movie
+    """
+    model = Movie
